83a60659722c1d7d6edbbf46157aa4ea50a2453d
sumanthsura <ssura@wpi.edu>
2023-04-25 19:15:13 -0400

    ruler falling data + graphs

--- arduino.py
+++ arduino.py
@@ -1,13 +1,26 @@
 from main import comp, simpleFallingWithoutFile
 import matplotlib.pyplot as plt
+import time
+st = time.time()
+
 
 times = []
 accelerations = []
 temp_times = []
 temp_accelerations = []
 
+def global_minimum(t_vals, a_vals):
+    min_i = 0
+    min_a = a_vals[0]
+    for i in range(len(a_vals)):
+        if (a_vals[i]) < min_i:
+            min_a = a_vals[i]
+            min_i = i
+    return [min_a, min_i]
 
-with open("arduino-data/falling/FallOnBedArduino.TXT") as f:
+fileName = "arduino-data/falling/ruler.TXT"
+
+with open(fileName) as f:
     lines = f.readlines()
     length = len(lines)
     j = 1
@@ -19,9 +32,11 @@ with open("arduino-data/falling/FallOnBedArduino.TXT") as f:
             plt.xlabel("Time (s)") 
             plt.ylabel("Acceleration (m/s^2)")
             plt.scatter(temp_times, temp_accelerations)
-            #plt.savefig(f"images/data/arduino/falling/fallingData{j}.png")
+            plt.savefig(f"images/data/arduino/falling/fallingRulerData{j}.png")
             plt.show()
-            print(f"falling of file {j}: {simpleFallingWithoutFile(temp_times, temp_accelerations)}")
+            s = simpleFallingWithoutFile(temp_times, temp_accelerations)
+            # global_minimum(temp_times, temp_accelerations)
+            print(f"falling of file {j}: {s}")
             temp_times.clear()
             temp_accelerations.clear()
             j += 1
@@ -36,3 +51,22 @@ with open("arduino-data/falling/FallOnBedArduino.TXT") as f:
 print(len(times))
 print(len(accelerations))
 
+class Entry:
+    def __init__(self, time, acc_x, acc_y, acc_z, rot_x, rot_y, rot_z):
+        self.time = time
+        self.acc_x = acc_x
+        self.acc_y = acc_y
+        self.acc_z = acc_z
+        self.rot_x = rot_x
+        self.rot_y = rot_y
+        self.rot_z = rot_z
+        acc_magnitude = comp(acc_x, acc_y, acc_x)
+        rot_magnitude = comp(rot_x, rot_y, rot_z)
+
+
+
+
+
+#ending code
+end = time.time()
+print(f"arduino.py runtime: {end-st}s")
