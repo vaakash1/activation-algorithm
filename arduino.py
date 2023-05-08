1590d8bef1521fa5070ca914311ad1ba936241c9
sumanthsura <ssura@wpi.edu>
2023-05-08 13:14:05 -0400

    minor arduino py changes

--- arduino.py
+++ arduino.py
@@ -16,7 +16,7 @@ def global_minimum(t_vals, a_vals):
         if (a_vals[i]) < min_i:
             min_a = a_vals[i]
             min_i = i
-    return [min_a, min_i]
+    return [min_a, min_i] # returns min acceleration and minimum i
 
 fileName = "arduino-data/walking/walkingWithFannyPack.TXT"
 mid = fileName.split("/")[-2]
